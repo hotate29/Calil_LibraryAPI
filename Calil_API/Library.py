5ae70e3b8ff8c9ed3c0da45bed3917bd4c0df19f
hotate29 <hotate_oc@yahoo.co.jp>
2020-12-05 19:31:21 +0900

    dataclassとかいう先進技術 #5

---
+++ Calil_API/Library.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+import dataclasses
+from typing import Any, Tuple
+
+
+@dataclass
+class Library:
+    category: str
+    city: str
+    short: str
+    libkey: str
+    pref: str
+    primary: bool
+    faid: Any
+    geocode:str
+    geocode_tuple:Tuple[float,float] = dataclasses.field(init=False)
+    systemid: str
+    address: str
+    libid: str
+    tel: str
+    systemname: str
+    isil: Any
+    post: str
+    url_pc: str
+    formal: str
+
+    def __post_init__(self):
+        self.geocode_tuple = tuple(map(float,self.geocode.split(",",1)))
